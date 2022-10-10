84bdd0572647c15297be2cf160b44dc3f5583c59
Sondre Schirmer-Mikalsen <sondremikalsen1@gmail.com>
2022-10-10 14:57:51 +0200

    Board spawn oculus fixed

--- Masterprosjekt/Assets/BoardSpawnScript.cs
+++ Masterprosjekt/Assets/BoardSpawnScript.cs
@@ -1,13 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class BoardSpawnScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-
     public GameObject leftHand;
     public GameObject RightHand;
+    public GameObject cam;
 
     public GameObject boardPrefab;
 
@@ -28,18 +26,20 @@ public class BoardSpawnScript : MonoBehaviour
             
         var leftPos = leftHand.GetComponent<Transform>().position;
         var rightPos = RightHand.GetComponent<Transform>().position;
+       var camRotation = cam.GetComponent<Transform>().rotation;
 
         //float boardScale = Mathf.Abs(rightPos.x-leftPos.x);
         float boardScale = Vector3.Distance(leftPos, rightPos);
 
         Debug.Log("Board in meter" + boardScale);
 
-        GameObject newBoard = Instantiate(boardPrefab );
-
+       
+        Debug.Log(camRotation.y*180);
+        var camForward = new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z);
+        GameObject newBoard = Instantiate(boardPrefab, Vector3.Lerp(leftPos, rightPos, 0.5f)+ camForward*boardScale/2, Quaternion.LookRotation(camForward, Vector3.up));
+       
         newBoard.GetComponent<Transform>().localScale = new Vector3(boardScale, boardScale,boardScale);
-        newBoard.GetComponent<Transform>().position = new Vector3(leftPos.x+boardScale/2, leftPos.y, leftPos.z+boardScale/2);
-
-        //newBoard.GetComponent<Transform>().position = new Vector3(3,3,3);
+    
         }
     }
 }
